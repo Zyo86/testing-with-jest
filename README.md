be5adc6fc424af5a134009a373277c05f1d182d3
Zyo86 <zyothebest86@gmail.com>
2021-03-29 20:53:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# testing-with-jest
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/testing-with-jest)
---
+++ src/TestingApp/TestingApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TestingApp() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Just a component to explain one concept !</p>
+    </div>
+  );
+}
